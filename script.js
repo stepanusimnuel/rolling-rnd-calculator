708f83c68c3c3f0fa23d767a1e31d12711454ae4
debelva <145679938+debelva@users.noreply.github.com>
2023-10-19 20:16:23 +0700

    Update script.js
    mengatasi input sembarangan

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 const result = document.querySelector('.res');
 
-document.addEventListener('click', function(e) 
-{
-	if(e.target.type == "button") 
-  {
-		if(e.target.value == "cls") 
-    {
-			result.value = '';
-	  } 
-    else if(e.target.value == "del") 
-    {
-			result.value = result.value.toString().slice(0, -1);
-		} 
-    else if (e.target.value == "=")
-    {
-			result.value = eval(result.value);
-		} 
-    else 
-    {
-			result.value += e.target.value;
-		}
-	}
-});	
+document.addEventListener('click', function(e) {
+ if(e.target.type == "button") {
+  if(e.target.value == "cls") {
+   result.value = '';
+  } else if(e.target.value == "del") {
+   result.value = result.value.toString().slice(0, -1);
+  } else if (e.target.value == "="){
+   try {
+    result.value = eval(result.value);
+   } catch(err){
+    result.value = "error pak";
+   }
+    
+  }
+  else result.value += e.target.value;
+ }
+});
 
